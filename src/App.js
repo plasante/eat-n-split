b8d9984077a76c995fe17b94565c3e0013efc459
Pierre Lasante <pierre.lasante@videotron.ca>
2024-02-09 10:58:10 -0500

    Finished the static part

--- src/App.js
+++ src/App.js
@@ -25,7 +25,10 @@ export default function App() {
       <div className="sidebar">
         <FriendsList />
         <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -81,6 +84,31 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>💰 Your expense</label>
+      <input type="text" />
+
+      <label>💰 X's expense</label>
+      <input type="text" disabled />
+
+      <label>😥 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
     </form>
   );
 }
